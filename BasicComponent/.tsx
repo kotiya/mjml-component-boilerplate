b696aff303b5d613a9c97dfe5a0276b98c6df359
Weiran Li <weiran@second.dev>
2023-08-09 21:03:35 -0700

    Second[Agent] mjml to react email

---
+++ BasicComponent/.tsx
@@ -0,0 +1,10 @@
+
+    import React from 'react';
+
+    const BasicComponent = ({ children }) => {
+
+      return <div color="green" >{ children }</div>;
+    };
+
+    export default BasicComponent;
+    
---
+++ ImageText/.tsx
@@ -0,0 +1,10 @@
+
+    import React from 'react';
+
+    const ImageText = ({ children }) => {
+
+      return <div background-color="#DD685B" image-src="https://mjml.io/assets/img/mjml-logo.png" image-width="100px" font-size="24px" image-position="left" >{ children }</div>;
+    };
+
+    export default ImageText;
+    
