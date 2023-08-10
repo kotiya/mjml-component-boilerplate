8121e3e61a53f788db01835d72ee65a57d494fa7
Weiran Li <weiran@second.dev>
2023-08-09 22:50:13 -0700

    Second[Agent] mjml to react email

---
+++ BasicComponent.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  Body,
+  Button,
+  Column,
+  Container,
+  Font,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Row,
+  Section,
+  Tailwind,
+  Text,
+} from "@react-email/components";
+
+const BasicComponent = () => {
+  return (
+    <Html>
+      <Head>
+        <Font />
+      </Head>
+      <Body>
+        <Container>
+          <Section>
+            <Row>
+              <Column>
+                <Heading>Basic Component</Heading>
+                <Text>
+                  This is a basic component using the @react-email/components
+                  library.
+                </Text>
+                <Button>Click me</Button>
+                <Hr />
+                <Img src="https://example.com/image.jpg" alt="Example Image" />
+                <Link href="https://example.com">Visit Example Website</Link>
+              </Column>
+            </Row>
+          </Section>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+export default BasicComponent;
