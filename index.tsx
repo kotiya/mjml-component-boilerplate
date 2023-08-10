e10f2bcfa365824b9a97da85c9d576a7bd7f00d4
Weiran Li <weiran@second.dev>
2023-08-09 17:45:50 -0700

    Second[Agent] mjml to react email

---
+++ index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+    import { HTML, Body, Section, Column, Text } from '@react-email/components';
+    
+    export function IndexEmail(props) {
+      const { url } = props;
+    
+      return (
+        <HTML><Body><Section><Column><div styles={"color":"green"}>I'm among stars</div></Column></Section><div styles={"backgroundColor":"#DD685B","fontSize":"24px"} attributes={"imageSrc":"https://mjml.io/assets/img/mjml-logo.png","imageWidth":"100px","imagePosition":"left"}>MJML rules! hello modified</div><div><Text styles={"color":"blue","fontSize":"10px"}>A super nice layout component</Text></div></Body></HTML>
+      );
+    }
+    
+    export default IndexEmail;
